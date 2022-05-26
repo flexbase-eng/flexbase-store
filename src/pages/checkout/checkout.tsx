810f0c70d6833b01502c8aa1e123a9daa9a9a732
Peilong Du <peilongdu@PeilongacStudio.attlocal.net>
2022-05-26 11:31:36 -0400

    updated with test merchant api key

--- src/pages/checkout/checkout.tsx
+++ src/pages/checkout/checkout.tsx
@@ -47,7 +47,7 @@ const CheckoutPage = () => {
                 <Space h="md" />
 
                 <Group position="right">
-                    <pay-with-flexbase apikey="fe" amount={(total + tax).toString()} callback="/done" session="session1"/>
+                    <pay-with-flexbase apikey="067e554c-119f-4741-8697-e59a08e42f51" amount={(total + tax).toString()} callback="/paymentSuccess" session={`session${Date.now.toString()}`}/>
                     {/* <Button variant="outline" color="flexbase-orange" rightIcon={<FlexbaseLogo height={25} fill="#ff5745" />}>
                         <Text size="xl">Pay with</Text>
                     </Button> */}
