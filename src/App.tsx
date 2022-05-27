704b4e82cbe5af18dccfa676d16c23ecaae9dad6
pdu2022 <104773868+pdu2022@users.noreply.github.com>
2022-05-27 10:42:36 -0400

    Readd (#8)
    * remove pay with flexbase
    
    * re-add to resolve dependency update
    
    Co-authored-by: Peilong Du <peilongdu@PeilongacStudio.attlocal.net>

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import MainPage from './pages/main/main';
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-// import { default as PayWithFlexbase } from '@flexbase-eng/pay-with-flexbase'
+import { default as PayWithFlexbase } from '@flexbase-eng/pay-with-flexbase'
 
-// declare global {
-//     namespace JSX {
-//         interface IntrinsicElements {
-//             'pay-with-flexbase': PayWithFlexbaseProps
-//         }
-//     }
-// }
-//
-// interface PayWithFlexbaseProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-//     apikey: string,
-//     amount: string,
-//     callback: string,
-//     session: string,
-// }
-//
-// customElements.get('pay-with-flexbase') || customElements.define('pay-with-flexbase', PayWithFlexbase);
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'pay-with-flexbase': PayWithFlexbaseProps
+        }
+    }
+}
+
+interface PayWithFlexbaseProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+    apikey: string,
+    amount: string,
+    callback: string,
+    session: string,
+}
+
+customElements.get('pay-with-flexbase') || customElements.define('pay-with-flexbase', PayWithFlexbase);
 
 function App() {
   return (
--- src/pay-with-flexbase.d.ts
+++ src/pay-with-flexbase.d.ts
@@ -1,4 +1,4 @@
-// declare module '@flexbase-eng/pay-with-flexbase' {
-//     declare const PayWithFlexbase: CustomElementConstructor;
-//     export default PayWithFlexbase
-// }
+declare module '@flexbase-eng/pay-with-flexbase' {
+    declare const PayWithFlexbase: CustomElementConstructor;
+    export default PayWithFlexbase
+}
